5907f95d6c1f93ef064697b4f2e2ee4b44f7d4a6
XtraCheese <127965783+XtraCheese@users.noreply.github.com>
2024-06-17 15:11:19 +0300

    Update mininingsim.lua

--- mininingsim.lua
+++ mininingsim.lua
@@ -128,6 +128,8 @@ function SellBlocks(HumanoidRootPart: BasePart)
 		Remote:FireServer("SellItems", {{}})
 	end
 	
+	print("GetCoins():", GetCoins(), "Rebirths.Value:", Rebirths.Value)
+	
 	if GetCoins() >= (Rebirths.Value + 1) * 10000000 then
 		Remote:FireServer("Rebirth", {{}})
 	end
